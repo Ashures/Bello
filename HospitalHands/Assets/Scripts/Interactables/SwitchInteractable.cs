71101d2436a6c1acc5cdaa8e938c6968012b4017
TechnicallyAlive <caseyqcc@gmail.com>
2022-07-24 10:19:46 +1200

    Added Button functionality

--- HospitalHands/Assets/Scripts/Interactables/SwitchInteractable.cs
+++ HospitalHands/Assets/Scripts/Interactables/SwitchInteractable.cs
@@ -9,17 +9,21 @@ public class SwitchInteractable : MonoBehaviour
 
     public Sprite onSprite;
     public Sprite offSprite;
+    public bool isOn = false; 
 
     public SpriteRenderer spriteRenderer;
+    public string playerWithSwitch;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switchEntered = true;
+        playerWithSwitch = collision.gameObject.name;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         switchEntered = false;
+        playerWithSwitch = null;
     }
 
     private void Start()
@@ -31,24 +35,24 @@ public class SwitchInteractable : MonoBehaviour
 
     private void Update()
     {
-         if (Input.GetKeyDown("e") && switchEntered)
+         if ((Input.GetKeyDown("e") && switchEntered && playerWithSwitch == "Player") || (Input.GetKeyDown(KeyCode.Keypad0) && switchEntered && playerWithSwitch == "Player2"))
             {
                 if (SwitchObject != null)
                 {
                     Debug.Log("Switch Pressed");
                     SwitchObject.gameObject.SetActive(!SwitchObject.gameObject.activeInHierarchy);
-                    
-                 
                 }
 
-                if (spriteRenderer.sprite == onSprite)
-                {
-                    spriteRenderer.sprite = offSprite;
-                }
-
-                if (spriteRenderer.sprite == offSprite)
+                if (isOn == false)
                 {
                     spriteRenderer.sprite = onSprite;
+                    isOn = true;
+                }
+
+                else if (isOn == true)
+                {
+                    spriteRenderer.sprite = offSprite;
+                    isOn = false;
                 }
         }
     }
