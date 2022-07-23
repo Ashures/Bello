f5f9053b6fb5ccc7172fe24cf60d3c5052781f93
TechnicallyAlive <caseyqcc@gmail.com>
2022-07-24 11:50:33 +1200

    Changed Introduction

--- HospitalHands/Assets/Scripts/Introduction.cs
+++ HospitalHands/Assets/Scripts/Introduction.cs
@@ -5,8 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class Introduction : MonoBehaviour
 {
+    public float timer = 1f;
+    public GameObject redPanel;
+
+
     public void LoadGame()
     {
+        StartCoroutine(IntroScene());
+
+    }
+
+    private IEnumerator IntroScene()
+    {
+        yield return RunFlash();
         SceneManager.LoadScene("SampleScene");
     }
+
+    private WaitForSeconds _waiter;
+    private IEnumerator RunFlash()
+    {
+        _waiter = new WaitForSeconds(timer);
+
+        redPanel.SetActive(true);
+        yield return _waiter;
+        redPanel.SetActive(false);
+
+        yield return _waiter;
+        redPanel.SetActive(true);
+        yield return _waiter;
+        redPanel.SetActive(false);
+    }
+
 }
