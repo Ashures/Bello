26eb3ce52245316efb69e8da638ea2a98ed443c6
TechnicallyAlive <caseyqcc@gmail.com>
2022-07-23 19:18:51 +1200

    Added Switch

--- HospitalHands/Assets/Scripts/Interactables/ButtonInteractable.cs
+++ HospitalHands/Assets/Scripts/Interactables/ButtonInteractable.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ButtonInteractable : InteractableObject
 {
+
+    private void Update()
+    {
+
+    }
+
     public GameObject SwitchObject;
 
     public override void OnInteracted(PlayerMove player)
--- HospitalHands/Assets/Scripts/Interactables/InteractableObject.cs
+++ HospitalHands/Assets/Scripts/Interactables/InteractableObject.cs
@@ -8,5 +8,4 @@ public class InteractableObject : MonoBehaviour
     {
         
     }
-
 }
