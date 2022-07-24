9514560a1dd1bd98ae3749f615138c7e54b0dcad
TechnicallyAlive <caseyqcc@gmail.com>
2022-07-24 12:38:14 +1200

    Body add lobby

--- HospitalHands/Assets/CollectTrophy.cs
+++ HospitalHands/Assets/CollectTrophy.cs
@@ -6,11 +6,21 @@ public class CollectTrophy : InteractableObject
 {
     public GameObject trophy;
 
+    public GameObject body;
+    public GameObject torso;
+    public GameObject lArm;
+    public GameObject rArm;
+    public GameObject lLeg;
+    public GameObject rLeg;
+
+
     // Start is called before the first frame update
     public override void OnInteracted(PlayerMove player)
     {
         if (player.GetComponent<PlayerMove>().trophy)
         {
+            LoadBody();
+
             Destroy(trophy);
             TrohpyManagement.player1Trophy = false;
             TrohpyManagement.player2Trophy = false;
@@ -20,9 +30,35 @@ public class CollectTrophy : InteractableObject
             Debug.Log(TrohpyManagement.player2Trophy);
             Debug.Log(player.GetComponent<PlayerMove>().trophy);
 
-
-
             LevelManager.NextLevel += 1;
+        }
+    }
+    public void LoadBody()
+    {
+        if(trophy.name == "Torso")
+        {
+            torso.SetActive(true);
+            TrohpyManagement.torso = true;
+        }
+        if (trophy.name == "LArm")
+        {
+            lArm.SetActive(true);
+            TrohpyManagement.torso = true;
+        }
+        if (trophy.name == "RArm")
+        {
+            rArm.SetActive(true);
+            TrohpyManagement.torso = true;
+        }
+        if (trophy.name == "LLeg")
+        {
+            lLeg.SetActive(true);
+            TrohpyManagement.torso = true;
+        }
+        if (trophy.name == "RLeg")
+        {
+            rLeg.SetActive(true);
+            TrohpyManagement.torso = true;
         }
     }
 }
